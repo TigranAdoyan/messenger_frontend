70bfffbaedea94d7376f8bac327acab4363ab25d
tigranadoyan <tigran.adoyan@mail.ru>
2022-10-20 12:27:41 +0400

    into messenger

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,135 +1,145 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import io from 'socket.io-client';
 
 const events = {
-   send_message: 'send_message',
-   receive_msg: 'receive_msg',
-   sync_msg: 'sync_msg',
-   join_room: 'join_room',
-   sync_user_info: 'sync_user_info'
-}
+    send_message: 'send_message',
+    receive_msg: 'receive_msg',
+    sync_msg: 'sync_msg',
+    join_room: 'join_room',
+    sync_user_info: 'sync_user_info'
+};
 
-const socket = io('http://localhost:3334/message', {auth: {token: 'ioabwpd98haliuwbdikauwujbd'}});
+const socket = io('http://localhost:3334/message', { auth:  { token: 'awdawd' } });
 
 socket.on('connect', () => {
-   console.log('socket connected');
-})
+    console.log('socket connected');
+});
 
-// socket.on('connect_error', (e) => {
-//    console.error(e);
-// })
+socket.on('connect_error', err => {
+    console.log(err)
+});
+socket.on('connect_failed', err => {
+    console.log(err)
+});
+socket.on('disconnect', err => {
+    console.log(err)
+});
+
+
+
+
 
 function Chat() {
-   const [messagesList, setMessagesList] = useState([]);
-   const [currentMessage, setMessage] = useState('');
-   const [user, setUser] = useState({username: ''});
-   const [currentRoom, setRoom] = useState('');
+    const [messagesList, setMessagesList] = useState([]);
+    const [currentMessage, setMessage] = useState('');
+    const [user, setUser] = useState({ username: '' });
+    const [currentRoom, setRoom] = useState('');
 
-   const onChange = useCallback((e) => {
-      if (e.target.name === 'message') {
-         setMessage(e.target.value);
-      } else if (e.target.name === 'room') {
-         setRoom(e.target.value);
-      }
-   }, [])
+    const onChange = useCallback((e) => {
+        if (e.target.name === 'message') {
+            setMessage(e.target.value);
+        } else if (e.target.name === 'room') {
+            setRoom(e.target.value);
+        }
+    }, [])
 
-   const onSubmit = useCallback((e) => {
-      if (e.type === 'click' || e.key === 'Enter') {
-         if (e.target.name === 'message') {
-            socket.emit(events.send_message, {
-               message: currentMessage,
-            })
-         } else if (e.target.name === 'room') {
-            socket.emit(events.join_room, {
-               room: currentRoom,
-            })
-         }
-      }
-   }, [currentMessage, currentRoom])
+    const onSubmit = useCallback((e) => {
+        if (e.type === 'click' || e.key === 'Enter') {
+            if (e.target.name === 'message') {
+                socket.emit(events.send_message, {
+                    message: currentMessage,
+                })
+            } else if (e.target.name === 'room') {
+                socket.emit(events.join_room, {
+                    room: currentRoom,
+                })
+            }
+        }
+    }, [currentMessage, currentRoom])
 
-   const _syncMessagesHandler = useCallback((messages) => {
-      setMessagesList(messages)
-   }, []);
+    const _syncMessagesHandler = useCallback((messages) => {
+        setMessagesList(messages)
+    }, []);
 
-   const _syncUserInfoHandler = useCallback((user) => {
-      setUser(user)
-   }, []);
+    const _syncUserInfoHandler = useCallback((user) => {
+        setUser(user)
+    }, []);
 
-   const _receiveHandler = useCallback((message) => {
-      setMessagesList(prevState => {
-         return [
-            ...prevState,
-            message
-         ]
-      })
-   }, []);
+    const _receiveHandler = useCallback((message) => {
+        setMessagesList(prevState => {
+            return [
+                ...prevState,
+                message
+            ]
+        })
+    }, []);
 
-   useEffect(() => {
-      socket.on(events.sync_msg, _syncMessagesHandler)
-      socket.on(events.sync_user_info, _syncUserInfoHandler)
-      socket.on(events.receive_msg, _receiveHandler)
+    useEffect(() => {
+        socket.on(events.sync_msg, _syncMessagesHandler)
+        socket.on(events.sync_user_info, _syncUserInfoHandler)
+        socket.on(events.receive_msg, _receiveHandler)
 
-      return () => {
-         socket.off(events.sync_msg, _syncMessagesHandler)
-         socket.off(events.sync_user_info, _syncUserInfoHandler)
-         socket.off(events.receive_msg, _receiveHandler)
-      }
-   }, [])
+        return () => {
+            socket.off(events.sync_msg, _syncMessagesHandler)
+            socket.off(events.sync_user_info, _syncUserInfoHandler)
+            socket.off(events.receive_msg, _receiveHandler)
+        }
+    }, [])
 
-   return (
-       <div className="App">
-          <div>
-             Messanger
-          </div>
+    return (
+        <div className="App">
+            <div>
+                Messanger
+            </div>
 
-          <div>
-             username: {user.username || '-'}
-          </div>
+            <div>
+                username: {user.username || '-'}
+            </div>
 
-          <div className="message_container">
-             {
-                messagesList.map(({id, message}, index) => {
-                   return <span key={id} className="message_row">
+            <div className="message_container">
+                {
+                    messagesList.map(({ id, message }, index) => {
+                        return <span key={id} className="message_row">
                                 <span> sender: {id} :=> </span>
                                 <span> {message} </span>
                             </span>
-                })
-             }
-          </div>
+                    })
+                }
+            </div>
 
-          <div className="interaction_container">
-             <div className="interaction_container_row">
-                <input
-                    type="text"
-                    name="message"
-                    placeholder="Message"
-                    value={currentMessage}
-                    onChange={onChange}
-                    onKeyDown={onSubmit}
-                />
+            <div className="interaction_container">
+                <div className="interaction_container_row">
+                    <input
+                        type="text"
+                        name="message"
+                        placeholder="Message"
+                        value={currentMessage}
+                        onChange={onChange}
+                        onKeyDown={onSubmit}
+                    />
 
-                <button name='message' onClick={onSubmit}>
-                   Send
-                </button>
-             </div>
+                    <button name='message' onClick={onSubmit}>
+                        Send
+                    </button>
+                </div>
 
-             <div className="interaction_container_row">
-                <input
-                    type="text"
-                    name="room"
-                    placeholder="Room"
-                    value={currentRoom}
-                    onChange={onChange}
-                    onKeyDown={onSubmit}
-                />
+                <div className="interaction_container_row">
+                    <input
+                        type="text"
+                        name="room"
+                        placeholder="Room"
+                        value={currentRoom}
+                        onChange={onChange}
+                        onKeyDown={onSubmit}
+                    />
 
-                <button name='room' onClick={onSubmit}>
-                   Join
-                </button>
-             </div>
-          </div>
-       </div>
-   );
+                    <button name='room' onClick={onSubmit}>
+                        Join
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Chat;
